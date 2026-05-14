480e4197bb03d26d50f23ce7a569c41d39e2d335
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-05-15 00:57:57 +0530

    Add Input and Throw_And_Throws classes demonstrating exception handling and user input

---
+++ 01-java-basics/Input.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Input {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a number");
+        System.in.read();
+    }
+}
