eb9b6f4db690845c853a3f9b04eeb3fe6ae4a11f
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-07-09 22:38:08 +0530

    Add initial setup for REST API project using Spring Boot
    Created .gitignore files for IntelliJ IDEA and Maven project
    
    Added VCS configuration for Git
    
    Introduced README.md with an overview of REST APIs
    
    Set up Maven wrapper for consistent builds
    
    Created pom.xml with Spring Boot dependencies
    
    Implemented main application class RestApiApplication
    
    Added application.properties for application name configuration
    
    Included basic test class RestApiApplicationTests

---
+++ 08-Backend-development/day-02-REST-API-and-Database/dbConnectivity/src/main/java/com/dbConnectivity/dbConnectivity/DbConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.dbConnectivity.dbConnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectivityApplication.class, args);
+	}
+
+}
---
+++ 08-Backend-development/day-02-REST-API-and-Database/dbConnectivity/src/test/java/com/dbConnectivity/dbConnectivity/DbConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbConnectivity.dbConnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
